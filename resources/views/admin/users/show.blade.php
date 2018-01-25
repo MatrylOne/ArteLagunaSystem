8accfbb9a022765c2f54400cb25b0d0c9f3be8dd
Jakub Nadolny <matryl1@me.com>
2018-01-25 14:13:49 +0100

    Poprawa wyświetlania użytkownika

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -1,31 +1,75 @@
 @extends('layouts.app')
 
 @section('content')
-@section('jumbo-title', 'Wyświetlanie użytkownika')
-@section('jumbo-description', $user->firstName.' '.$user->lastName)
 @include('inc.admin-navigation')
-@include('inc.jumbotron')
 
-<div class="container">
+<!--FormularzRejestracji-->
+<div class="container mt-5">
+    <h2>Dane użytkownika</h2>
+    <table class="table">
+        <thead class="thead-primary">
+        <tr>
+            <th scope="col">Imię</th>
+            <th scope="col">Nazwisko</th>
+            <th scope="col">Email</th>
+            <th scope="col">Kraj</th>
+        </tr>
+        </thead>
+        <tbody>
+            <td>{{$user->firstName}}</td>
+            <td>{{$user->lastName}}</td>
+            <td>{{$user->email}}</td>
+            <td>{{$user->country}}</td>
+        </tbody>
+    </table>
+</div>
+<div class="container mt-5">
     @if($user->works->count() > 0)
-        @foreach($user->works as $work)
-            @if($work->award != null)
-                <h2>Moja nagroda</h2>
-                <b>{{$work->award->name}}</b>
-                <p>Miejsce {{$work->award->place}} w kategorii {{$work->award->category->name}} za pracę
-                    "{{$work->name}}"</p>
-            @endif
-            <h2>Praca</h2>
-            <p>{{$work->name}}</p>
-            @php ($frends = $work->users->reject(function($author) use ($user){return $author->id == $user->id;}))
-            @if($frends->count() > 0)
-                <h2>Współautorzy</h2>
+        <h2>Prace użytkownika</h2>
+        <table class="table">
+            <thead class="thead-primary">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nazwa pracy</th>
+                <th scope="col">Kategoria</th>
+                <th scope="col">Edycja</th>
+                <th scope="col">Współutorzy</th>
+                <th scope="col">Miejsce</th>
+                <th scope="col">Nagroda</th>
+            </tr>
+            </thead>
+            <tbody>
+            @php($i = 1)
+            @foreach($user->works as $work)
+                @php ($frends = $work->users->reject(function($author) use ($user){return $author->id == $user->id;}))
 
-                @foreach($frends as $frend)
-                    <p>{{$frend->firstName.' '.$frend->lastName}}</p>
-                @endforeach
-            @endif
-        @endforeach
+                <tr>
+                    <td>{{$i}}</td>
+                    <td>{{$work->name}}</td>
+                    <td>{{$work->category->name}}</td>
+                    <td>{{$work->category->edition->year}}</td>
+                    <td>
+                        @if($frends->count() > 0)
+                            @foreach($frends as $frend)
+                                {{$frend->firstName.' '.$frend->lastName}}<br>
+                            @endforeach
+                        @endif
+                    </td>
+                    <td>
+                        @if($work->award != null)
+                            {{$work->award->place}}
+                        @endif
+                    </td>
+                    <td>
+                        @if($work->award != null)
+                            {{$work->award->name}}
+                        @endif
+                    </td>
+                </tr>
+                @php($i++)
+            @endforeach
+            </tbody>
+        </table>
     @endif
 </div>
 @endsection
